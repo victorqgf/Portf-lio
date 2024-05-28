674f452ae9124753914249c43d14fc9c5d2b241f
Victor <152400005+victorqgf@users.noreply.github.com>
2024-05-28 11:27:29 -0300

    Update validador_de_cpf.py

--- validador_de_cpf.py
+++ validador_de_cpf.py
@@ -14,4 +14,4 @@ second_digit = str(calculo_cpf(user_cpf[:10], 11))
 
 cpf_gerado = f'{user_cpf[:9]}{first_digit}{second_digit}'
 
-# print('CPF VÁLIDO' if cpf_gerado == user_cpf else 'CPF INVÁLIDO')
+print('CPF VÁLIDO' if cpf_gerado == user_cpf else 'CPF INVÁLIDO')
